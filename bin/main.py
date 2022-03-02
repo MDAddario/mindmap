30efbd5e8854a3f3489b91fb226f53e4df403c6d
MDAddario <michael.lindner.daddario@gmail.com>
2022-03-01 20:47:54 -0500

    Adds endpoint for spec #3

--- bin/main.py
+++ bin/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 
+
 # Define app
 app = Flask(__name__)
 
@@ -10,6 +11,7 @@ def post_id():
     identity = request.json['id']
     return f"Created {identity}"
 
+
 # SPEC 2: post a new leaf to ID
 @app.route("/<identity>", methods=['POST'])
 def post_leaf(identity):
@@ -17,6 +19,7 @@ def post_leaf(identity):
     path = request.json['path']
     text = request.json['text']
     return f"Added {path} with {text} for {identity}"
+
 
 # SPEC 4: get the whole tree from ID
 @app.route("/<identity>", methods=['GET'])
@@ -26,8 +29,7 @@ def get_tree(identity):
 
 
 # SPEC 3: get a single leaf from ID
-
-@app.route("/<iden>/<path:path>", methods=['POST'])
-def home_page(iden, path):
-    assert request.method == 'POST'
-    return f"{iden} followed by {path} + {request.json['id']}"
+@app.route("/<identity>/<path:path>", methods=['GET'])
+def get_leaf(identity, path):
+    
+    return f"Requested {path} from {identity}"
