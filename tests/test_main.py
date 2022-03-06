c5efcf2888ad8bc74810ec40f3bcbcddc62aea74
MDAddario <michael.lindner.daddario@gmail.com>
2022-03-06 16:11:15 -0500

    Updates and comments for complete unit testing suite

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,12 @@ import pytest
 
 from bin import main, persistance
 
+'''
+Note that this code implicity tests the persistance
+of the mindmap app because a session is not created
+and preserved across tests
+'''
+
 # Delete pre-existing data
 persistance.file_db.unlink(missing_ok=True)
 
@@ -9,10 +15,7 @@ persistance.file_db.unlink(missing_ok=True)
 @pytest.fixture()
 def app():
 
-    main.app.config.update({
-        "TESTING": True,
-    })
-
+    main.app.config.update({"TESTING": True})
     yield main.app
 
 
@@ -23,68 +26,117 @@ def client(app):
 
 def test_post_id(client):
 
+    # Create ID
     response = client.post("/", json={"id": "vegetables"})
     assert response.status_code == 200
+    assert response.data == b'Mind map creation successful.'
 
+    # Ensure ID cannot be duplicated
     response = client.post("/", json={"id": "vegetables"})
     assert response.status_code == 400
+    assert response.data == b'ID "vegetables" already has a mind map.'
 
+    # Create another ID
     response = client.post("/", json={"id": "fruits"})
     assert response.status_code == 200
+    assert response.data == b'Mind map creation successful.'
 
+    # Ensure ID cannot be duplicated
     response = client.post("/", json={"id": "fruits"})
     assert response.status_code == 400
+    assert response.data == b'ID "fruits" already has a mind map.'
 
 
 def test_post_leaf(client):
 
+    # Test missing ID
     response = client.post("/candy", json={
         "path": "i/like/candy",
         "text": "we have no candy tree"
         })
     assert response.status_code == 400
+    assert response.data == b'ID "candy" does not have a mind map.'
 
+    # Add complex leaf
     response = client.post("/vegetables", json={
         "path": "i/like/green/peppers",
         "text": "stuff them with rice"
         })
     assert response.status_code == 200
+    assert response.data == b'Mind map leaf addition successful.'
 
+    # Add complex leaf
     response = client.post("/vegetables", json={
         "path": "i/like/green/salads",
         "text": "they're healthy"
         })
     assert response.status_code == 200
+    assert response.data == b'Mind map leaf addition successful.'
 
+    # Add complex leaf
     response = client.post("/vegetables", json={
         "path": "i/hate/cauliflower",
         "text": "what a strange veggie"
         })
     assert response.status_code == 200
+    assert response.data == b'Mind map leaf addition successful.'
 
+    # Add complex leaf
     response = client.post("/vegetables", json={
         "path": "i/hate/brocoli",
         "text": "small trees"
         })
     assert response.status_code == 200
+    assert response.data == b'Mind map leaf addition successful.'
+
+    # Overwrite text of existing leaf
+    response = client.post("/vegetables", json={
+        "path": "i/hate/brocoli",
+        "text": "we can overwrite the text"
+        })
+    assert response.status_code == 200
+    assert response.data == b'Mind map leaf addition successful.'
 
 
 def test_get_leaf(client):
 
+    # Test missing ID
+    response = client.get("/candy/i/like/candy/corn")
+    assert response.status_code == 400
+    assert response.data == b'ID "candy" does not have a mind map.'
+
+    # Get text from leaf
     response = client.get("/vegetables/i/like/green/peppers")
     assert response.json == {
         "path": "i/like/green/peppers",
         "text": "stuff them with rice"
         }
 
+    # Get overwritten text from leaf
     response = client.get("/vegetables/i/hate/brocoli")
     assert response.json == {
         "path": "i/hate/brocoli",
-        "text": "small trees"
+        "text": "we can overwrite the text"
         }
+
+    # Try inexistent path
+    response = client.get("/vegetables/i/like/orange/plums")
+    assert response.status_code == 400
+    assert response.data == b'Path "i/like/orange/plums" does not exist in "vegetables" mind map.'
 
 
 def test_get_tree(client):
+
+    # Test missing ID
+    response = client.get("/candy")
+    assert response.status_code == 400
+    assert response.data == b'ID "candy" does not have a mind map.'
+
+    # Pretty print empty ID
+    response = client.get("/fruits")
+    assert response.data == b'root'
+
+    # Pretty print complex ID
     response = client.get("/vegetables")
     assert response.data == b"""root/
 \ti/
