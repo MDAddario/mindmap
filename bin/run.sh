c409793fef1902e209c059d079fac6ac296db564
MDAddario <michael.lindner.daddario@gmail.com>
2022-03-08 21:23:22 -0500

    Fixes typoes in header

--- bin/run.sh
+++ bin/run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-####################################
-# Launch the mind map web service  #
-#                                  #
-# This script should be ran in the #
-# root folder of the proeject      #
-####################################
+######################################
+# Launch the mind map web service    #
+#                                    #
+# This script should be ran from the #
+# root folder of the project         #
+######################################
 
 export FLASK_APP=./service/main
 flask run
